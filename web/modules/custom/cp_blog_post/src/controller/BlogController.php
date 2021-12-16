fca6e95bfc82cde2c475a23c596579c5a4991d79
gregory rameaux <gregory.rameaux@3wa.io>
2021-12-16 09:45:35 +0100

    TP affichage article de blog dans /blog

---
+++ web/modules/custom/cp_blog_post/src/controller/BlogController.php
@@ -0,0 +1,89 @@
+<?php
+namespace Drupal\cp_blog_post\Controller;
+use Drupal;
+use Drupal\Core\Controller\ControllerBase;
+/**
+ * An example controller.
+ */
+class BlogController extends ControllerBase
+{
+    /**
+     * Returns a render-able array for a test page.
+     */
+    public function content()
+    {
+        $limit = 5;
+        $build = [
+            '#markup' => '<p>' . $this->t('Pas de résultat') . '</p>',
+        ];
+        $entityTypeManager = Drupal::entityTypeManager();
+        $nodeStorage = $entityTypeManager->getStorage('node');
+        $query = $nodeStorage->getQuery();
+        $query
+            ->condition('type', 'blog_post')
+            ->condition('status', true)
+            ->sort('changed', 'DESC');
+        $query->pager($limit);
+        $nids = $query->execute();
+        if (!empty($nids)) {
+            $nodeObjects = $nodeStorage->loadMultiple($nids);
+            $nodeViewBuilder = $entityTypeManager->getViewBuilder('node');
+            $view_mode = 'teaser';
+            //$nodes = $nodeViewBuilder->viewMultiple($nodeObjects, $view_mode);
+            $nodes = [];
+            foreach ($nodeObjects as $node) {
+                $nodes[] = $nodeViewBuilder->view($node, $view_mode);
+            }
+            $build = [
+                'wrapper' => [
+                    '#type' => 'container',
+                    '#attributes' => [
+                        'class' => ['BlogList-wrapper'],
+                    ],
+                    'nodes' => [
+                        '#theme' => 'cp_blog_post_blog_list',
+                        '#nodes' => $nodes,
+                    ],
+                    'pager' => [
+                        '#type' => 'pager',
+                    ],
+                ],
+            ];
+        }
+        return $build;
+    }
+}
+// $nodes = Drupal::entityTypeManager()
+//     ->getStorage('node')
+//     ->loadMultiple($nodeIds);
+// dpm($nodeStorage);
+// $query = $nodeStorage->getQuery();
+// $query->condition('type', 'blog_post');
+// $nodes = $query->execute();
+// dpm($nodeStorage);
+/* 1/ methode : filtre de selection pour affichage de type "blog" */
+// $nodes = $nodeStorage->loadByProperties([
+//     'type' => 'blog_post',
+//     'status' => true,
+// ]);
+// dpm($nodes);
+
+/* 1/ methode : filtre de selection pour affichage de type "blog" */
+// $query = $nodeStorage->getQuery();
+// $query->condition('type', 'blog_post');
+// $nodes = $query->execute();
+
+// if (!empty($nids)) {
+//     $nodes = $nodeStorage->loadMultiple($nids);
+//     dpm($nodes);
+// }
+
+// $countQuery = clone $query;
+// $count = $countQuery->count()->execute();
+// $nids = $query->execute();
+// dpm($count);
+
+// // Do something with your variables here.
+// $myText = 'toto titi tata !';
+// $myNumber = 44;
+// $myArray = [1, 2, 3];
